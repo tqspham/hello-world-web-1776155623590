61db3add5dbf2263b328b8ceeef4493b557bb0f8
tqspham <tqs.pham@gmail.com>
2026-04-14 10:34:04 +0200

    Adding app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { HelloWorld } from '@/components/HelloWorld';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <HelloWorld />
+    </main>
+  );
+}
